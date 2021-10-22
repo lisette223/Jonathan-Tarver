300308ed70e0b22c0d217c9890102a02b2e582c8
LisetteAntigua <antigua223@gmail.com>
2021-10-22 09:36:44 -0400

    comment time

--- Jonathan Tarver/ViewController.swift
+++ Jonathan Tarver/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfield: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func stbuttion(_ sender: Any) {
+        
+        label.text = textfield.text
+    }
+    
+    @IBAction func ctbutton(_ sender: Any) {
+
+        label.text = ""
+    }
+
+    
 }
 
